0d5d55de1e1d526a49718be5cff6b968944f5b5b
techabhi08 <garg.abhinay8602@gmail.com>
2023-07-04 11:22:34 +0530

    feat: ✨ fixed the CORS error

--- src/main/java/com/springrestapi/springrestapi/controller/MyController.java
+++ src/main/java/com/springrestapi/springrestapi/controller/MyController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,26 +23,31 @@ public class MyController {
 	@Autowired
 	private PersonService personService;
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/persons")
 	public List<Person> getPersons(){
 		return this.personService.getPersons();
 	}
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/persons/{personId}")
 	public Person getPerson(@PathVariable String personId){
 		return this.personService.getPerson(Long.parseLong(personId));
 	}
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@PostMapping("/persons")
 	public Person addPerson(@RequestBody Person person) {
 		return this.personService.addPerson(person);
 	}
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@PutMapping("/persons")
 	public Person editPerson(@RequestBody Person person) {
 		return this.personService.updatePerson(person);
 	}
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@DeleteMapping("/persons/{personId}")
 	public ResponseEntity<HttpStatus> deleteCourse(@PathVariable String personId) {
 		try {
